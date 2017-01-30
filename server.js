06e5a9639c0cb5efec02bfda3b698f68b5d5b3c1
Naomi Kudren <N.Kudren@amaze.com>
2017-01-30 14:47:08 +0000

    progress private messages

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var sio = require("socket.io");
 var express = require("express");
 var port = 3000;
 var onlineUsers = [];
@@ -66,7 +65,7 @@ io.on('connection', function(socket) {
         for (var i = 0; i < onlineUsers.length; i++) {
             if (onlineUsers[i].name == msg) {
                 io.emit("private chat", msg);
-                console.log("hello");
+
             }
 
         }
@@ -75,7 +74,12 @@ io.on('connection', function(socket) {
 
     socket.on("private message", function(msg) {
         var nickname = socket.nickname;
-        io.emit("private message", nickname + ": " + msg);
+        for(var i =0; i< onlineUsers.length; i++){
+          var specific = onlineUsers[i].socketid;
+        }
+        io.to(specific).emit("private message", nickname + ": " + msg);
+
+        console.log(specific);
 
     });
 
@@ -85,26 +89,24 @@ io.on('connection', function(socket) {
 
 //Uploading document
 
-app.post('/upload', function(req, res) {
-    var form = new formidable.IncomingForm();
-    form.multiples = false;
-    form.uploadDir = path.join(__dirname, "/uploads");
-    form.on('file', function(field, file) {
-        fs.rename(file.path, path.join(form.uploadDir, file.name));
-    });
-    form.on('error', function(err) {
-        console.log('An error has occured: \n' + err);
-    });
-
-    form.on('end', function() {
-        res.end('success');
-    });
-
-    form.parse(req);
-});
+// app.post('/upload', function(req, res) {
+//     var form = new formidable.IncomingForm();
+//     form.multiples = false;
+//     form.uploadDir = path.join(__dirname, "/uploads");
+//     form.on('file', function(field, file) {
+//         fs.rename(file.path, path.join(form.uploadDir, file.name));
+//     });
+//     form.on('error', function(err) {
+//         console.log('An error has occured: \n' + err);
+//     });
+//
+//     form.on('end', function() {
+//         res.end('success');
+//     });
+//
+//     form.parse(req);
+// });
 
 http.listen(port, function() {
     console.log('listening on *:' + port);
 });
-
-console.log(numberOnline);
